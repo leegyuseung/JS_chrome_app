81ceca8bb892838844a070520713f3cfc26a6da2
leegyuseung <lgs4002@naver.com>
2022-06-30 18:17:27 +0900

    clock 완료

---
+++ js/clock.js
@@ -0,0 +1,12 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+// 처음부터 00:00:00이 나오지 않도록 하기 위한 장치
+getClock();
+setInterval(getClock, 1000);
